0db3ed9446d855aab146c83d34cd73181df064a1
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2023-03-06 10:00:39 +0200

    Introductory text added

--- src/Form.js
+++ src/Form.js
@@ -7,7 +7,8 @@ const Form = ()=> {
     return(
         <div className="container">
             <div className="image-block"></div>
-            
+            <h1>Purchase Ticket</h1>
+            <p className="introduction-text">Fill out the form below to purchase your ticket for the Open window Festival 2022</p>
             
             <Link to="/final">
             <button>Purchase</button>
