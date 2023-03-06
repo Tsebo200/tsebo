d7bd12890e35c868c438a9da1fba51607d6f49cc
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2023-03-06 10:57:41 +0200

    Submitting Now

--- src/Form.js
+++ src/Form.js
@@ -12,17 +12,25 @@ const Form = ()=> {
             <p className="introduction-text">Fill out the form below to purchase your ticket for the Open window Festival 2022</p>
            
             <form>
-                <label>Your Name & Surname</label><br></br>
-                <input type="text" placeholder="Test Student"></input><br></br><br></br>
+                <label>Your Name & Surname</label>
+                <div className= "input-breaker"></div>
+                <input type="text" placeholder="Test Student"></input><br></br>
 
-                <label>Your Email</label><br></br>
-                <input type="text" placeholder="test@openwindow.co.za"></input><br></br><br></br>
+                <label>Your Email</label>
+                <div className= "input-breaker"></div>
+                <input type="text" placeholder="test@openwindow.co.za"></input><br></br>
 
-                <label>Ticket Amount</label><br></br>
-                <input type="number" placeholder="3"></input><br></br><br></br>
+                <label>Ticket Amount</label>
+                <div className= "input-breaker"></div>
+                <input type="number" placeholder="3"></input><br></br>
 
-                <label>Ticket type</label><br></br>
-                <input type="text" placeholder="Kiddies"></input><br></br><br></br>
+                <label>Ticket type</label>
+                <div className= "input-breaker"></div>
+                <select placeholder="Kiddies">
+                    <option>Kiddies</option>
+                     <option>Adult</option>
+                     <option>Teens</option>
+                </select><br></br>
             </form>
             <Link to="/final">
             <button>Purchase</button>
