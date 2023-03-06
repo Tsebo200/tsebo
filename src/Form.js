6db8b87ce0886fb029710901016c61949f4297e7
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2023-03-06 10:12:19 +0200

    Moving onto Final Form
    Will fix the alignment a bit later

--- src/Form.js
+++ src/Form.js
@@ -9,7 +9,19 @@ const Form = ()=> {
             <div className="image-block"></div>
             <h1>Purchase Ticket</h1>
             <p className="introduction-text">Fill out the form below to purchase your ticket for the Open window Festival 2022</p>
-            
+            <form>
+                <label>Your Name & Surname</label><br></br>
+                <input type="text" placeholder="Test Student"></input><br></br><br></br>
+
+                <label>Your Email</label><br></br>
+                <input type="text" placeholder="test@openwindow.co.za"></input><br></br><br></br>
+
+                <label>Ticket Amount</label><br></br>
+                <input type="number" placeholder="3"></input><br></br><br></br>
+
+                <label>Ticket type</label><br></br>
+                <input type="text" placeholder="Kiddies"></input><br></br><br></br>
+            </form>
             <Link to="/final">
             <button>Purchase</button>
             </Link>
