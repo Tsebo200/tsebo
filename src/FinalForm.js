ff903c109b15c326937f4210b77babacd986ac6f
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2023-03-06 10:28:20 +0200

    completed Final Price Page

--- src/FinalForm.js
+++ src/FinalForm.js
@@ -1,9 +1,28 @@
 import React from "react";
-import './index.css'
+import './index.css';
+import { Link } from "react-router-dom";
 
 const FinalForm = () => {
     return(
-        <div className="Background"></div>
+        <div className="final-container">
+            <div className="space-breaker"></div>
+            <div className="final-image-block"></div>
+
+            <br></br>
+            <hr></hr>
+
+            <h3>Final Price</h3>
+            <h2>R 450.00</h2>
+
+            <p className="thank-you-msg">Thank you test student or purchasing 3 tickest. We sent your tickets to <br></br> 
+             test@openwindow.co.za.
+            </p>
+
+            <Link to="/">
+            <center><button>One More Ticket🙂</button></center>
+            </Link>
+        </div>
+        
     )
 }
 
